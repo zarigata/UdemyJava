11b1d51fe1dd76b0fb7ffdeb754aa80cbc3f3aa7
Zarigata <carlos@valleteck.com>
2023-12-24 01:54:44 -0300

    UwU

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Good Morning");
+        double y = 32.15321812;
+        System.out.printf("%.3f%n", y);
+        System.out.printf("%.4f%n", y);
+        Locale.setDefault(Locale.US);
     }
 
 }
