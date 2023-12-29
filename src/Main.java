e0495135281d23b0d6ea578afc45757ae29a1f35
Zarigata <carlos@valleteck.com>
2023-12-29 00:36:08 -0300

    UwU

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,24 @@ public class Main {
         int age = 64;
         double money = 1200000000.0;
         System.out.printf("%s has %d years and owns U$ %.2f dollars" , name , age , money);
+        // EX de fixacao
+        String product1 = "computer";
+        String product2 = "Office Desk";
+
+        int ageF = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.234567;
+
+        System.out.println("Products");
+        System.out.printf("%s , witch the price is %f", product1 , price1);
+        System.out.printf("%s , witch the price is %f", product2 , price2);
+
+        System.out.printf("Record: %d ");
+
     }
 
 }
